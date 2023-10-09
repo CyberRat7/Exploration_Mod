dcc2254e0ffbe2931c4f357b641294d47f25e42c
w4t3rcs <uimanovmaks@gmail.com>
2023-10-09 19:38:58 +0200

    Some clean-ups

--- src/main/java/net/explorationmod/cyber2000/ExplorationMod.java
+++ src/main/java/net/explorationmod/cyber2000/ExplorationMod.java
@@ -7,61 +7,38 @@ import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExplorationMod.MODID)
-public class ExplorationMod
-{
-    public static final String MODID = "exploration_mod";
+@Mod(ExplorationMod.MOD_ID)
+public class ExplorationMod {
+    public static final String MOD_ID = "exploration_mod";
+    public static final Logger logger = LogUtils.getLogger();
 
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public ExplorationMod()
-    {
+    public ExplorationMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-
         modEventBus.addListener(this::commonSetup);
-
-
         MinecraftForge.EVENT_BUS.register(this);
-
-
         modEventBus.addListener(this::addCreative);
-
-
-
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {
-
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
     }
-//test
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
         }
     }
 }
