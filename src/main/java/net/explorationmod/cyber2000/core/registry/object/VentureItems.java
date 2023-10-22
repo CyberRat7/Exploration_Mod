1d80f2178b042b0455d34bd36cd377139570779a
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-10-22 23:26:34 +0200

    Added frozen disc item

--- src/main/java/net/explorationmod/cyber2000/core/registry/object/VentureItems.java
+++ src/main/java/net/explorationmod/cyber2000/core/registry/object/VentureItems.java
@@ -19,9 +19,9 @@ public class VentureItems {
     public static final RegistryObject<Item> FROZEN_TRIDENT = ITEMS.register("frozen_trident", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> FROZEN_EMERALD = ITEMS.register("frozen_emerald", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> FROZEN_SALMON = ITEMS.register("frozen_salmon", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> FROZEN_DISC = ITEMS.register("frozen_disc", () -> new RecordItem(3, VentureSoundEvents.FROZEN_DISC_SOUND, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 400));
     public static final RegistryObject<Item> FROZEN_COD = ITEMS.register("frozen_cod", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> FROZEN_NAUTILUS_SHELL = ITEMS.register("frozen_nautilus_shell", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FROZEN_DISC = ITEMS.register("frozen_disc", () -> new RecordItem(3, VentureSoundEvents.FROZEN_DISC_SOUND, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 400));
     public static void register(IEventBus eventBus) {
         LoggingRegistry.register(ITEMS, eventBus);
     }
