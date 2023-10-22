e6ee27b8b7504e7ffe3388ed6bb217eef03687b5
w4t3rcs <uimanovmaks@gmail.com>
2023-10-22 21:43:02 +0200

    Added CreativeTabs support

--- src/main/java/net/explorationmod/cyber2000/Venture.java
+++ src/main/java/net/explorationmod/cyber2000/Venture.java
@@ -1,6 +1,7 @@
 package net.explorationmod.cyber2000;
 
 import com.mojang.logging.LogUtils;
+import net.explorationmod.cyber2000.core.registry.object.VentureCreativeTabs;
 import net.explorationmod.cyber2000.core.registry.object.VentureItems;
 import net.explorationmod.cyber2000.core.registry.object.VenturePaintings;
 import net.explorationmod.cyber2000.core.registry.object.VentureSoundEvents;
@@ -37,6 +38,7 @@ public class Venture {
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        VentureCreativeTabs.register(event);
     }
 
     @SubscribeEvent
---
+++ src/main/java/net/explorationmod/cyber2000/core/util/creative/CreativeTabAdder.java
@@ -0,0 +1,62 @@
+package net.explorationmod.cyber2000.core.util.creative;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class CreativeTabAdder {
+    private final BuildCreativeModeTabContentsEvent event;
+
+    public CreativeTabAdder(BuildCreativeModeTabContentsEvent event) {
+        this.event = event;
+    }
+
+    public void addAllToIngredients(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.INGREDIENTS, registries);
+    }
+
+    public void addAllToCombat(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.COMBAT, registries);
+    }
+
+    public void addAllToTools(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.TOOLS_AND_UTILITIES, registries);
+    }
+
+    public void addAllToFood(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.FOOD_AND_DRINKS, registries);
+    }
+
+    public void addAllToBuildingBlocks(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.BUILDING_BLOCKS, registries);
+    }
+
+    public void addAllToNaturalBlocks(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.NATURAL_BLOCKS, registries);
+    }
+
+    public void addAllToColoredBlocks(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.COLORED_BLOCKS, registries);
+    }
+
+    public void addAllToFunctionalBlocks(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.FUNCTIONAL_BLOCKS, registries);
+    }
+
+    public void addAllToRedstoneBlocks(List<Supplier<? extends ItemLike>> registries) {
+        addAllToTab(CreativeModeTabs.REDSTONE_BLOCKS, registries);
+    }
+
+    public void addAllToTab(ResourceKey<CreativeModeTab> tab, List<Supplier<? extends ItemLike>> registries) {
+        registries.forEach(registry -> addToCreativeTab(tab, registry));
+    }
+
+    private void addToCreativeTab(ResourceKey<CreativeModeTab> tab, Supplier<? extends ItemLike> registryObject) {
+        if (event.getTabKey() == tab) event.accept(registryObject);
+    }
+}
