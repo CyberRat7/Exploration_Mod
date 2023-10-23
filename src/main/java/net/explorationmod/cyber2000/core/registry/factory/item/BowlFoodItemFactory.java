f94c1a0078f8e2dba74b859d87d010e692f7be03
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-10-23 14:45:03 +0200

    Fixed nettle soup stacking to 64

--- src/main/java/net/explorationmod/cyber2000/core/registry/factory/item/BowlFoodItemFactory.java
+++ src/main/java/net/explorationmod/cyber2000/core/registry/factory/item/BowlFoodItemFactory.java
@@ -8,7 +8,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public interface BowlFoodItemFactory extends DefaultItemFactory {
     static RegistryObject<BowlFoodItem> create(String name, int nutrition, float saturation) {
-        return DefaultItemFactory.createTyped(name, () -> new BowlFoodItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(nutrition).saturationMod(saturation).build())));
+        return DefaultItemFactory.createTyped(name, () -> new BowlFoodItem(new Item.Properties().stacksTo(1).food(new FoodProperties.Builder().nutrition(nutrition).saturationMod(saturation).build())));
 
     }
 }
