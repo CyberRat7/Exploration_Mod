3537f356d1c717bbeb54ff681c87bb06d078251b
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-10-23 14:28:48 +0200

    Finished nettle soup and added BowlFoodItemFactory

---
+++ src/main/java/net/explorationmod/cyber2000/core/registry/factory/item/BowlFoodItemFactory.java
@@ -0,0 +1,14 @@
+package net.explorationmod.cyber2000.core.registry.factory.item;
+
+import net.explorationmod.cyber2000.core.registry.factory.DefaultItemFactory;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+
+public interface BowlFoodItemFactory extends DefaultItemFactory {
+    static RegistryObject<BowlFoodItem> create(String name, int nutrition, float saturation) {
+        return DefaultItemFactory.createTyped(name, () -> new BowlFoodItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(nutrition).saturationMod(saturation).build())));
+
+    }
+}
