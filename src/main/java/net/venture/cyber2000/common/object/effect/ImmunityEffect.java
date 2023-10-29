8213cd340aadb3f4482da6790951d2a5356f5cdc
w4t3rcs <uimanovmaks@gmail.com>
2023-10-29 23:04:12 +0100

    Updated ImmunityEffect

--- src/main/java/net/venture/cyber2000/common/object/effect/ImmunityEffect.java
+++ src/main/java/net/venture/cyber2000/common/object/effect/ImmunityEffect.java
@@ -2,27 +2,10 @@ package net.venture.cyber2000.common.object.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.MilkBucketItem;
-import net.minecraft.world.level.Level;
-
-import java.util.Collection;
 
 public class ImmunityEffect extends MobEffect {
     public ImmunityEffect(MobEffectCategory pCategory, int pColor) {
         super(pCategory, pColor);
-    }
-
-    @Override
-    public void applyEffectTick(LivingEntity livingEntity, int pAmplifier) {
-        Collection<MobEffectInstance> effects = livingEntity.getActiveEffects();
-        effects.forEach(effectInstance -> {
-            MobEffect effect = effectInstance.getEffect();
-            if (!(effect instanceof ImmunityEffect)) {
-                livingEntity.removeEffect(effect);
-            }
-        });
     }
 
     @Override
---
+++ src/main/java/net/venture/cyber2000/core/event/VentureEffectEvents.java
@@ -0,0 +1,20 @@
+package net.venture.cyber2000.core.event;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.MobEffectEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.venture.cyber2000.Venture;
+import net.venture.cyber2000.core.registry.object.VentureEffects;
+
+@Mod.EventBusSubscriber(modid = Venture.MOD_ID)
+public class VentureEffectEvents {
+    @SubscribeEvent
+    public static void applyEffects(MobEffectEvent.Applicable event) {
+        LivingEntity entity = event.getEntity();
+        if (entity.hasEffect(VentureEffects.IMMUNITY.get())) {
+            event.setResult(Event.Result.DENY);
+        }
+    }
+}
