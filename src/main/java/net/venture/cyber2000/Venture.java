23ab890852c2b70b1f65d49f4d26ab01430e230b
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-10-29 12:23:41 +0100

    Added immunity potions and tipped arrows

--- src/main/java/net/venture/cyber2000/Venture.java
+++ src/main/java/net/venture/cyber2000/Venture.java
@@ -1,8 +1,12 @@
 package net.venture.cyber2000;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.FlowerPotBlock;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.venture.cyber2000.common.object.potion.VentureBrewingRecipe;
 import net.venture.cyber2000.core.registry.object.*;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -41,6 +45,11 @@ public class Venture {
     private void commonSetup(final FMLCommonSetupEvent event) {
         event.enqueueWork(() -> {
             ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(VentureBlocks.STINGING_NETTLE.getId(), VentureBlocks.POTTED_STINGING_NETTLE);
+
+            BrewingRecipeRegistry.addRecipe(new VentureBrewingRecipe(Potions.AWKWARD,
+                    VentureItems.NETTLE_LEAVES.get(), VenturePotions.IMMUNITY_POTION.get()));
+            BrewingRecipeRegistry.addRecipe(new VentureBrewingRecipe(VenturePotions.IMMUNITY_POTION.get(),
+                    Items.REDSTONE, VenturePotions.LONG_IMMUNITY_POTION.get()));
         });
     }
 
--- src/main/java/net/venture/cyber2000/core/registry/factory/DefaultPotionFactory.java
+++ src/main/java/net/venture/cyber2000/core/registry/factory/DefaultPotionFactory.java
@@ -1,5 +1,6 @@
 package net.venture.cyber2000.core.registry.factory;
 
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,7 +13,9 @@ public interface DefaultPotionFactory extends ObjectFactory {
     static RegistryObject<Potion> create(String name, MobEffectInstance mobEffectInstance) {
         return POTIONS.register(name, () -> new Potion(mobEffectInstance));
     }
-
+     static RegistryObject<Potion> create(String id, Supplier<MobEffect> effect, int duration) {
+        return POTIONS.register(id, () -> new Potion(new MobEffectInstance(effect.get(), duration)));
+    }
     private static RegistryObject<Potion> create(String name, Supplier<Potion> potionSupplier) {
         return POTIONS.register(name, potionSupplier);
     }
