e99000a104929b5f48ec96233bfae54de8eccae7
CyberRat7 <cyber.rat.wheel@gmail.com>
2023-11-26 11:36:40 +0100

    Removed ancient bow from creative menu

--- src/main/java/net/venturer/cyber2000/core/registry/object/VenturerCreativeTabs.java
+++ src/main/java/net/venturer/cyber2000/core/registry/object/VenturerCreativeTabs.java
@@ -13,7 +13,6 @@ public class VenturerCreativeTabs {
                 .processIngredients(VenturerItems.FROZEN_NAUTILUS_SHELL, VenturerItems.FROZEN_COD, VenturerItems.FROZEN_EMERALD, VenturerItems.FROZEN_TRIDENT,
                         VenturerItems.FROZEN_SALMON, VenturerItems.FROZEN_PUFFERFISH, VenturerItems.FROZEN_IRON_HORSE_ARMOR,
                         VenturerItems.NETTLE_LEAVES, VenturerItems.CHILL_ARMOR_TRIM_SMITHING_TEMPLATE)
-                .processCombat(VenturerItems.ANCIENT_BOW)
                 .processTools(VenturerItems.MUSIC_DISC_THAW)
                 .processFood(VenturerItems.NETTLE_SOUP)
                 .processNaturalBlocks(VenturerBlocks.STINGING_NETTLE, VenturerBlocks.BAG_OF_NETTLE_LEAVES);
