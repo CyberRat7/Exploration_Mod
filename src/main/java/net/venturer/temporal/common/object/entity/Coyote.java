1ff88278789545822aeef61c7ab88658dbf303e0
w4t3rcs <uimanovmaks@gmail.com>
2023-12-06 10:21:48 +0100

    Added picking up food and eating it!

--- src/main/java/net/venturer/temporal/common/object/entity/Coyote.java
+++ src/main/java/net/venturer/temporal/common/object/entity/Coyote.java
@@ -5,18 +5,21 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.TimeUtil;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.*;
 import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Husk;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.player.Player;
@@ -25,29 +28,37 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.EnumSet;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 public class Coyote extends Animal implements NeutralMob {
-    private static final EntityDataAccessor<Integer> DATA_REMAINING_ANGER_TIME = SynchedEntityData.defineId(Wolf.class, EntityDataSerializers.INT);
-    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
+    private static final EntityDataAccessor<Integer> DATA_REMAINING_ANGER_TIME = SynchedEntityData.defineId(Coyote.class, EntityDataSerializers.INT);
+    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(21, 40);
+    private static final Predicate<? super ItemEntity> ALLOWED_ITEMS = (itemEntity -> {
+        ItemStack item = itemEntity.getItem();
+        return item.is(Items.ROTTEN_FLESH);
+    });
     @Nullable
     private UUID persistentAngerTarget;
 
-    public Coyote(EntityType<? extends Animal> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
+    public Coyote(EntityType<? extends Animal> entityType, Level level) {
+        super(entityType, level);
     }
 
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
         this.goalSelector.addGoal(4, new LeapAtTargetGoal(this, 0.4F));
+        this.goalSelector.addGoal(4, new SearchForFoodGoal());
         this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.addGoal(7, new BreedGoal(this, 1.0D));
         this.goalSelector.addGoal(8, new WaterAvoidingRandomStrollGoal(this, 1.0D));
         this.goalSelector.addGoal(10, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(10, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(3, (new HurtByTargetGoal(this)).setAlertOthers());
-        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+        this.targetSelector.addGoal(4, new NearestAttackableTargetGoal<>(this, Player.class, 10, false, false, this::isAngryAt));
         this.targetSelector.addGoal(7, new NearestAttackableTargetGoal<>(this, Zombie.class, false));
         this.targetSelector.addGoal(7, new NearestAttackableTargetGoal<>(this, Husk.class, false));
         this.targetSelector.addGoal(8, new ResetUniversalAngerTargetGoal<>(this, true));
@@ -158,5 +169,77 @@ public class Coyote extends Animal implements NeutralMob {
     @Override
     public void startPersistentAngerTimer() {
         this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
+    }
+
+    @Override
+    @SuppressWarnings("DataFlowIssue")
+    public void tick() {
+        super.tick();
+        if (this.isAlive()) {
+            AttributeInstance movementSpeed = this.getAttribute(Attributes.MOVEMENT_SPEED);
+            if (this.isAngry()) {
+                float spedUpValue = 1.2F;
+                movementSpeed.setBaseValue(spedUpValue);
+            } else {
+                float baseValue = 0.3F;
+                movementSpeed.setBaseValue(baseValue);
+            }
+        }
+    }
+
+    class SearchForFoodGoal extends Goal {
+        public SearchForFoodGoal() {
+            this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+        }
+
+        @Override
+        public boolean canUse() {
+            if (!Coyote.this.getItemBySlot(EquipmentSlot.MAINHAND).isEmpty()) {
+                return false;
+            } else if (Coyote.this.getTarget() == null && Coyote.this.getLastHurtByMob() == null) {
+                if (Coyote.this.getRandom().nextInt(reducedTickDelay(10)) != 0) {
+                    return false;
+                } else {
+                    List<ItemEntity> list = Coyote.this.level().getEntitiesOfClass(ItemEntity.class, Coyote.this.getBoundingBox().inflate(8.0D, 8.0D, 8.0D), Coyote.ALLOWED_ITEMS);
+                    return !list.isEmpty() && Coyote.this.getItemBySlot(EquipmentSlot.MAINHAND).isEmpty();
+                }
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public void tick() {
+            List<ItemEntity> list = Coyote.this.level().getEntitiesOfClass(ItemEntity.class, Coyote.this.getBoundingBox().inflate(8.0D, 8.0D, 8.0D), Coyote.ALLOWED_ITEMS);
+            ItemStack itemstack = Coyote.this.getItemBySlot(EquipmentSlot.MAINHAND);
+            if (itemstack.isEmpty() && !list.isEmpty()) {
+                Coyote.this.getNavigation().moveTo(list.get(0), 1.2F);
+            }
+
+            List<ItemEntity> items = Coyote.this.level().getEntitiesOfClass(ItemEntity.class, Coyote.this.getBoundingBox().inflate(0.5D, 0.5D, 0.5D), ALLOWED_ITEMS);
+            ItemEntity itemToEat = null;
+            if (!items.isEmpty()) itemToEat = items.get(0);
+            if (itemToEat != null) {
+                Coyote.this.pickUpItem(itemToEat);
+                boolean canEat = Coyote.this.onGround() && Coyote.this.getTarget() == null && Coyote.this.getLastHurtByMob() == null;
+                if (canEat) {
+                    ItemStack item = itemToEat.getItem();
+                    Coyote.this.eat(Coyote.this.level(), item);
+                    SoundEvent eatingSound = Coyote.this.getEatingSound(item);
+                    Coyote.this.playSound(eatingSound);
+                    if (Coyote.this.getHealth() != Coyote.this.getAttribute(Attributes.MAX_HEALTH).getValue()) Coyote.this.setHealth(Coyote.this.getHealth() + 1);
+                    Coyote.this.removeEffect(MobEffects.HUNGER);
+                    Coyote.this.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+                }
+            }
+        }
+
+        @Override
+        public void start() {
+            List<ItemEntity> list = Coyote.this.level().getEntitiesOfClass(ItemEntity.class, Coyote.this.getBoundingBox().inflate(8.0D, 8.0D, 8.0D), Coyote.ALLOWED_ITEMS);
+            if (!list.isEmpty()) {
+                Coyote.this.getNavigation().moveTo(list.get(0), 1.2F);
+            }
+        }
     }
 }
