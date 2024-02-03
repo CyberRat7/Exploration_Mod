b432d15b140a2720db407065f9194aeb25b33caf
w4t3rcs <uimanovmaks@gmail.com>
2024-02-03 17:16:32 +0100

    Some small clean-ups!

--- src/main/java/net/venturer/temporal/client/model/CoyoteModel.java
+++ src/main/java/net/venturer/temporal/client/model/CoyoteModel.java
@@ -3,7 +3,6 @@ package net.venturer.temporal.client.model;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.WolfModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
