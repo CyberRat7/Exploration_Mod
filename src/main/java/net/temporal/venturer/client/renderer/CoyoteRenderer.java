fcd22eb00391753cf6ac85ff47931bba3c902032
jason13official <jason13official@gmail.com>
2024-03-18 20:31:19 -0400

    coyote shadow fix

--- src/main/java/net/temporal/venturer/client/renderer/CoyoteRenderer.java
+++ src/main/java/net/temporal/venturer/client/renderer/CoyoteRenderer.java
@@ -9,7 +9,7 @@ import net.temporal.venturer.common.object.entity.Coyote;
 
 public class CoyoteRenderer extends MobRenderer<Coyote, CoyoteModel<Coyote>> {
     public CoyoteRenderer(EntityRendererProvider.Context context) {
-        super(context, new CoyoteModel<>(context.bakeLayer(CoyoteModel.LAYER_LOCATION)), 0.25F);
+        super(context, new CoyoteModel<>(context.bakeLayer(CoyoteModel.LAYER_LOCATION)), 0.35F);
     }
 
     @Override
